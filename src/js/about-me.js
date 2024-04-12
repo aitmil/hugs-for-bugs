c60278c971cdf4b96de11eac70fe5558f2d2157e
Yuliia-Pluzhnik <yuliya.pluzhnik911@gmail.com>
2024-04-12 00:38:43 -0400

    add js, add style list, fixed html

---
+++ src/js/about-me.js
@@ -0,0 +1,65 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+function initAboutMeAccordion() {
+    new Accordion('.about-me-section__accordion-container', {
+        duration: 400,
+        showMultiple: false,
+        collapse: true,
+    });
+}
+
+initAboutMeAccordion(); 
+
+const accordionHeaders = document.querySelectorAll('.about-me-section__header');
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', function() {
+        const accordionItem = this.closest('.about-me-section__item');
+        const panel = accordionItem.querySelector('.about-me-section__panel');
+
+        if (panel) {
+            panel.classList.toggle('visually-hidden');
+        }
+    });
+});
+
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal', 
+    loop: true, 
+    navigation: {
+      nextEl: '.swiper-button-next', 
+      prevEl: '.swiper-button-prev',
+    },
+    keyboard: {
+      enabled: true, 
+    },
+    slidesPerView: 'auto', 
+    spaceBetween: 20, 
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        navigation: {
+          nextEl: null,
+          prevEl: null,
+        },
+      },
+     
+      992: {
+        slidesPerView: 3,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+      
+      1200: {
+        slidesPerView: 2,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+    },
+  });
