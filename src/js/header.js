5d6eca973a144016983b206befce0172957153dc
VolodymyrVasyliev <vasyliev.official@gmail.com>
2024-04-14 19:43:14 +0300

    update mobile-menu header

--- src/js/header.js
+++ src/js/header.js
@@ -24,3 +24,22 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenuWrapper.classList.remove('is-open');
   });
 });
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
+
+
