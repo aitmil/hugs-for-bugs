9302bf8a3dbf56d8ffd9b2815be53282c1d3361d
AleghSoroka <140161426+AleghSoroka@users.noreply.github.com>
2024-04-15 06:27:32 +0300

    Adaptive design is ready
    Still have to solve a couple of issues related to the Swiper and its buttons.

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,6 +3,7 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+<<<<<<< Updated upstream
 const projectsSwiper = new Swiper('.project-lists', {
   modules: [Navigation],
   keyboard: {
@@ -17,3 +18,109 @@ const projectsSwiper = new Swiper('.project-lists', {
     prevEl: '.swipe-btn-prev',
   },
 });
+=======
+// const projectsSwiper = new Swiper('.projects-swiper', {
+//   modules: [Navigation],
+//   keyboard: {
+//     enabled: true,
+//   },
+//   mousewheel: {
+//     invert: true,
+//   },
+//   watchOverflow: true,
+//   navigation: {
+//     nextEl: '.swiper-btn-next',
+//     prevEl: '.swiper-btn-prev',
+//   },
+// });
+
+const swiperContainer = document.querySelector('.projects-swiper-wrapper');
+// document.addEventListener('DOMContentLoaded', event => {
+//   swiperContainer.innerHTML = createMarkup(images);
+// });
+const seeProjectRef = 'https://github.com/aitmil/hugs-for-bugs/';
+const imgStorage = './img/project-section/';
+const images = [
+  {
+    mobile_1x: 'img_mob_1.jpg',
+    mobile_2x: 'img_mob_1@2x.jpg',
+    tablet_1x: 'img_tab_1.jpg',
+    tablet_2x: 'img_tab_1@2x.jpg',
+    desctop_1x: 'img_desc_1.jpg',
+    desctop_2x: 'img_desc_1@2x.jpg',
+  },
+  {
+    mobile_1x: 'img_mob_2.jpg',
+    mobile_2x: 'img_mob_2@2x.jpg',
+    tablet_1x: 'img_tab_2.jpg',
+    tablet_2x: 'img_tab_2@2x.jpg',
+    desctop_1x: 'img_desc_2.jpg',
+    desctop_2x: 'img_desc_2@2x.jpg',
+  },
+  {
+    mobile_1x: 'img_mob_3.jpg',
+    mobile_2x: 'img_mob_3@2x.jpg',
+    tablet_1x: 'img_tab_3.jpg',
+    tablet_2x: 'img_tab_3@2x.jpg',
+    desctop_1x: 'img_desc_3.jpg',
+    desctop_2x: 'img_desc_3@2x.jpg',
+  },
+];
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        mobile_1x,
+        mobile_2x,
+        tablet_1x,
+        tablet_2x,
+        desctop_1x,
+        desctop_2x,
+      }) =>
+        `<div class="projects-swiper-slide">
+        <div class="project-section-light-part">
+          <ul class="project-section-skills-list list">
+            <li class="project-section-list-skills-item">#react</li>
+            <li class="project-section-list-skills-item">#js</li>
+            <li class="project-section-list-skills-item">#node js</li>
+            <li class="project-section-list-skills-item">#git</li>
+          </ul>
+          <h3 class="project-section-title">
+            Programming Across Borders: Ideas, Technologies, Innovations
+          </h3>
+          <a
+            href="${seeProjectRef}"
+            class="see-project-ref"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="See project link"
+            >See project</a
+          >
+        </div>
+        <div class="project-section-dark-part">
+          <picture class="project-section-img">
+                  <source
+        srcset="${imgStorage}${desctop_1x} 1x, ${imgStorage}${desctop_2x} 2x"
+        media="(min-width: 1440px)"
+      />
+      <source
+        srcset="${imgStorage}${tablet_1x} 1x, ${imgStorage}${tablet_2x} 2x"
+        media="(min-width: 768px)"
+      />
+      <source
+        srcset="${imgStorage}${mobile_1x} 1x, ${imgStorage}${mobile_2x} 2x"
+        media="(min-width: 375px)"
+      />
+      <img
+        class="project-section-img"
+        src="${imgStorage}${mobile_1x}"
+        alt="Project example"
+      />
+          </picture>
+        </div>
+        </div>`
+    )
+    .join('');
+}
+>>>>>>> Stashed changes
