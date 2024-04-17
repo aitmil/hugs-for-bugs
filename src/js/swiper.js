f2e907f53d5d773463dc1883468cfd5fa365a2b7
Anna Taranchuk <taranchuk4@yahoo.com>
2024-04-18 01:44:06 +0300

    Update files

--- src/js/swiper.js
+++
@@ -1,5 +0,0 @@
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
