61f92c040651b7e4c9edf7781081b0463a14dc3d
VolodymyrVasyliev <vasyliev.official@gmail.com>
2024-04-17 09:55:53 +0300

    header mobile menu

--- src/js/header.js
+++ src/js/header.js
@@ -9,16 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
   const dropdownLinks = document.querySelectorAll('.dropdown');
 
   dropdownLinks.forEach(function (link) {
-    const dropdownContent = link.nextElementSibling;
-    const dropdownLinksContainer = link.parentElement.parentElement;
-    
-    
-    dropdownContent.classList.remove('is-open');
-    dropdownLinksContainer.classList.remove('is-open');
-    
     link.addEventListener('click', function (event) {
-      dropdownContent.classList.toggle('is-open'); 
-      dropdownLinksContainer.classList.toggle('is-open'); 
+      if (window.innerWidth > 768) {
+        const dropdownContent = link.nextElementSibling;
+        dropdownContent.classList.toggle('is-open');
+      }
     });
   });
 
@@ -27,19 +22,50 @@ document.addEventListener('DOMContentLoaded', function () {
   mobileMenuCloseBtn.addEventListener('click', function () {
     mobileMenuWrapper.classList.remove('is-open');
   });
+});
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+// ---------------------------------------------------------------
 
-      const target = document.querySelector(this.getAttribute('href'));
+document.addEventListener('DOMContentLoaded', function () {
+ 
+  const closeBtn = document.querySelector('.mobile-menu-close-btn');
+ 
+  const menuItems = document.querySelectorAll('.mobile-menu-link');
 
-      if (target) {
-        window.scrollTo({
-          top: target.offsetTop,
-          behavior: 'smooth',
-        });
-      }
+ 
+  function closeMenu() {
+    const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
+    mobileMenuWrapper.style.display = 'none';
+  }
+
+ 
+  closeBtn.addEventListener('click', function () {
+    closeMenu();
+  });
+
+ 
+  menuItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      closeMenu();
     });
+  });
+});
+
+
+// ---------------------------------------------------------------
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth',
+      });
+    }
   });
 });
