b03dd2ccfa9ed8f63323daca387b54b17fc45816
Anton81951 <antonbugaichuk@gmail.com>
2024-04-18 09:25:52 +0300

    1

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,20 +2,31 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 document.addEventListener("DOMContentLoaded", function() {
-    const sectors = document.querySelectorAll('.faq-sector');
+    const accordions = Array.from(document.querySelectorAll('.accordion-container'));
+    const accordion = new Accordion(accordions, {
+        duration: 400,
+        showFirstItem: false,
+        elementClass: 'faq-sector',
+        triggerClass: 'faq-accordion',
+        panelClass: 'faq-sector-description',
+        headerClass: 'faq-sector-title',
+        beforeOpen: (currElement) => {
+            const panelContent = currElement.querySelector('.faq-sector-description');
+            panelContent.style.display = 'block';
+        },
+        beforeClose: (currElement) => {
+            const panelContent = currElement.querySelector('.faq-sector-description');
+            panelContent.style.display = 'none';
+        },
+    });
+});
+
+  const sectors = document.querySelectorAll('.faq-sector');
 
     sectors.forEach(sector => {
         sector.addEventListener('click', function() {
             const arrow = this.querySelector('.faq-arrow');
-            const description = this.querySelector('.faq-sector-description');
 
             arrow.classList.toggle('rotate');
-            description.classList.toggle('visually-hidden');
         });
     });
-
-    new Accordion('.faq-section', {
-        duration: 400,
-        showFirstItem: false,
-    });
-});
